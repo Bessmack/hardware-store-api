06eddb6719e9f7db40ca20ab22c4b48d9238ee9d
Bessmack <jenwied4lif@gmail.com>
2026-06-10 21:54:44 +0300

    add: dependencies of airtel.go

--- internal/payments/airtel/airtel.go
+++ internal/payments/airtel/airtel.go
@@ -1,1 +1,15 @@
 package airtel
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/Bessmack/hardware-store-api/internal/payments"
+	"github.com/Bessmack/hardware-store-api/pkg/cache"
+	"github.com/Bessmack/hardware-store-api/pkg/logger"
+)
