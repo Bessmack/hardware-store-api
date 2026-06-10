0149571ae8ae4e2925af3fccff632e840be38934
Bessmack <jenwied4lif@gmail.com>
2026-06-10 22:00:40 +0300

    add: Provider implements payments.Provider for Airtel Money.

--- internal/payments/airtel/airtel.go
+++ internal/payments/airtel/airtel.go
@@ -19,3 +19,33 @@ const (
 	tokenCacheKey = "airtel:access_token"
 	tokenTTL      = 50 * time.Minute
 )
+
+// Provider implements payments.Provider for Airtel Money.
+// Docs: https://developers.airtel.africa/documentation
+type Provider struct {
+	clientID     string
+	clientSecret string
+	baseURL      string
+	httpClient   *http.Client
+	cache        *cache.Cache
+	storeCreds   payments.StoreCredentialsReader
+}
+
+type Config struct {
+	ClientID     string
+	ClientSecret string
+	BaseURL      string
+}
+
+func New(cfg Config, c *cache.Cache, storeCreds payments.StoreCredentialsReader) *Provider {
+	return &Provider{
+		clientID:     cfg.ClientID,
+		clientSecret: cfg.ClientSecret,
+		baseURL:      cfg.BaseURL,
+		httpClient:   &http.Client{Timeout: 15 * time.Second},
+		cache:        c,
+		storeCreds:   storeCreds,
+	}
+}
+
+func (p *Provider) Name() string { return "airtel" }
