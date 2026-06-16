9678c82e30fa2fa2dd41359138bfdf5c6bd7642d
Bessmack <jenwied4lif@gmail.com>
2026-06-16 10:32:02 +0300

    start: reports/repository

--- internal/reports/repository.go
+++ internal/reports/repository.go
@@ -1,1 +1,17 @@
 package reports
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/Bessmack/hardware-store-api/pkg/database"
+)
+
+type Repository struct {
+	db *database.DB
+}
+
+func NewRepository(db *database.DB) *Repository {
+	return &Repository{db: db}
+}
